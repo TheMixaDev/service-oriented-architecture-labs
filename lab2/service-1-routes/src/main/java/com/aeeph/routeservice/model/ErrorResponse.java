55836a0ae7c6934a9f5f7584012b1aefdc35d997
aleph <20299694+TheMixaDev@users.noreply.github.com>
2025-10-02 20:11:03 +0300

    I HATE WILDFLY

--- lab2/service-1-routes/src/main/java/com/aeeph/routeservice/model/ErrorResponse.java
+++ lab2/service-1-routes/src/main/java/com/aeeph/routeservice/model/ErrorResponse.java
@@ -10,6 +10,9 @@ public class ErrorResponse {
     private int status;
     private String message;
 
+    public ErrorResponse() {
+    }
+
     public ErrorResponse(int status, String message) {
         this.status = status;
         this.message = message;
