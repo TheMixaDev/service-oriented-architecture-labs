39c31ce3ea6d26bacba9b14b017a26950ba49ac7
aleph <20299694+TheMixaDev@users.noreply.github.com>
2025-10-02 20:24:18 +0300

    service 2 ssl ignore

--- lab2/service-2-navigator/src/main/java/com/aeeph/navigatorservice/config/RestTemplateConfig.java
+++ lab2/service-2-navigator/src/main/java/com/aeeph/navigatorservice/config/RestTemplateConfig.java
@@ -7,7 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +26,25 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        RestTemplate restTemplate = builder.build();
+        RestTemplate restTemplate;
+        try {
+            TrustStrategy acceptingTrustStrategy = (chain, authType) -> true;
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(csf)
+                    .build();
+
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(httpClient);
+
+            restTemplate = builder.requestFactory(() -> requestFactory).build();
+        } catch (Exception ex) {
+            restTemplate = builder.build();
+        }
         
         XmlMapper xmlMapper = new XmlMapper();
         MappingJackson2HttpMessageConverter xmlConverter = new MappingJackson2HttpMessageConverter(xmlMapper);
