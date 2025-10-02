ad5e8db5c8a84f125039a07c494e1daa7f4a6475
aleph <20299694+TheMixaDev@users.noreply.github.com>
2025-10-02 19:09:09 +0300

    service 1 wildfly

--- lab2/service-1-routes/src/main/java/com/aeeph/routeservice/RouteServiceApplication.java
+++ lab2/service-1-routes/src/main/java/com/aeeph/routeservice/RouteServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class RouteServiceApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(RouteServiceApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(RouteServiceApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(RouteServiceApplication.class, args);
